03dd2a4ccd4d274ef7562516d854d764fe3f11f9
Cody Njos <cody.njos@live.com>
2021-04-08 21:57:24 -0500

    amade incriment buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,36 @@
 <script>
 	export let name;
+	let count = 0;
+
+	function plus1() {
+		count += 1
+	}
+
+	function minus1() {
+		count -= 1
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
+	.countButton{
+		text-align: center;
+	}
+	p{
+		text-align: center;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
+	div{
+		text-align: center;
+	}
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
@@ -28,3 +44,15 @@
 		}
 	}
 </style>
+
+<p> Total count: {count} </p>
+<div >
+
+<button class="countButton" on:click={plus1}>
+	+1 
+</button>
+
+<button class="countButton" on:click={minus1}>
+-1
+</button>
+</div>
